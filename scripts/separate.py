efa2724772440a7298f445a2a5221cda6e91b2fe
passerbya <hanghang3103@163.com>
2023-12-04 19:07:33 +0800

    使用多线程

--- scripts/separate.py
+++ scripts/separate.py
@@ -5,7 +5,9 @@ import sys
 import shutil
 import demucs.separate
 import subprocess
+import threading
 from pathlib import Path
+from queue import Queue
 
 def delete_folder(pth) :
     for sub in pth.iterdir():
@@ -16,7 +18,35 @@ def delete_folder(pth) :
             sub.unlink()
     pth.rmdir()
 
+class TaskThread(threading.Thread):
+
+    def __init__(self, cuda_num):
+        threading.Thread.__init__(self)
+        self.queue_obj = Queue()
+        self.cuda_num = cuda_num
+
+    def add_data(self, cmd, outdir, wav_dest, wav_src):
+        self.queue_obj.put((cmd, outdir, wav_dest, wav_src))
+    def run(self):
+        while not self.queue_obj.empty():
+            (cmd, outdir, wav_dest, wav_src) = self.queue_obj.get()
+            print(cmd)
+            demucs.separate.main(["-d", f"cuda:{self.cuda_num}", "-n", "htdemucs_ft", "--two-stems", "vocals", "-o", str(outdir), wav_src])
+            ffmpeg_exe = '/usr/local/ffmpeg/bin/ffmpeg'
+            temp_path = outdir / 'htdemucs_ft' / wav_dest.stem / 'vocals.wav'
+            cmd = f'{ffmpeg_exe} -i "{temp_path}" -vn -ar 16000 -ac 1 -sample_fmt s16 -y "{wav_dest}"'
+            subprocess.check_output(cmd, shell=True).decode('utf-8', 'ignore')
+            try:
+                delete_folder(outdir / 'htdemucs_ft' / wav_dest.stem)
+            except:
+                pass
+
 def main():
+    thread_count = 2
+    ts = []
+    for i in range(thread_count):
+        ts.append(TaskThread(i))
+    i = 0
     for lang_dir in src.glob("*"):
         if not lang_dir.is_dir():
             continue
@@ -29,17 +59,13 @@ def main():
                 wav_dest.parent.mkdir(parents=True)
             outdir = src / 'temp'
             wav_src = str(srt).replace('/txt/', '/wav16k/').replace('.txt', '.wav')
-            cmd = f"source /etc/profile && /root/miniconda3/bin/demucs -n htdemucs_ft --two-stems=vocals -o {outdir} {wav_src}"
-            print(cmd)
-            demucs.separate.main(["-n", "htdemucs_ft", "--two-stems", "vocals", "-o", str(outdir), wav_src])
-            ffmpeg_exe = '/usr/local/ffmpeg/bin/ffmpeg'
-            temp_path = outdir / 'htdemucs_ft' / wav_dest.stem / 'vocals.wav'
-            cmd = f'{ffmpeg_exe} -i "{temp_path}" -vn -ar 16000 -ac 1 -sample_fmt s16 -y "{wav_dest}"'
-            subprocess.check_output(cmd, shell=True).decode('utf-8', 'ignore')
-            try:
-                delete_folder(outdir / 'htdemucs_ft' / wav_dest.stem)
-            except:
-                pass
+            cmd = f"source /etc/profile && /root/miniconda3/bin/demucs -d cuda:{i % thread_count} -n htdemucs_ft --two-stems=vocals -o {outdir} {wav_src}"
+            ts[i % thread_count].add_data(cmd, outdir, wav_dest, wav_src)
+            i += 1
+
+    print('-'*20, i)
+    for i in range(thread_count):
+        ts[i].start()
 
 if __name__ == "__main__":
     src = Path('/usr/local/share/38语料/语料盘/语料/4th_biz')
