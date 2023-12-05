1b5b0a27c634d61e599361c847291f5febdbdd9c
passerbya <hanghang3103@163.com>
2023-12-05 19:52:15 +0800

    增加输入参数，动态处理不同目录，处理文件名过长问题，处理windows下时间修改不成功问题

--- scripts/cp.py
+++ scripts/cp.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 import threading
+import argparse
 from pathlib import Path
 from queue import Queue
 
@@ -15,34 +16,59 @@ class TaskThread(threading.Thread):
 
     def add_data(self, _s, _d):
         self.queue_obj.put((_s, _d))
+
     def run(self):
-        while not self.queue_obj.empty():
+        while True:
             (_s, _d) = self.queue_obj.get()
-            shutil.copy(_s, _d)
+            if _s is None and _d is None:
+                break
             stinfo = os.stat(_s)
-            os.utime(_d, (stinfo.st_atime, stinfo.st_mtime))
+            if os.name == 'nt':
+                atime = int(stinfo.st_atime)
+                mtime = int(stinfo.st_mtime)
+            try:
+                shutil.copy2(_s, _d)
+                os.utime(_d, (atime, mtime))
+            except OSError:
+                _d = _d.parent / (_d.stem[:_d.stem.rfind(' ')] + _d.suffix)
+                print(_d)
+                try:
+                    shutil.copy2(_s, _d)
+                    os.utime(_d, (atime, mtime))
+                except OSError:
+                    _d = _d.parent / (_d.stem[:int(len(_d.stem)/2)] + _d.suffix)
+                    print(_d)
+                    shutil.copy2(_s, _d)
+                    os.utime(_d, (atime, mtime))
             print(f'{_d} ok')
 
 if __name__ == "__main__":
-    src = Path('/usr/local/data/jtubespeech')
-    dest = Path('/usr/local/share/ooo/38语料/语料盘/jtubespeech')
-    threadCount = 20
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src", type=str, default="D:/38语料/语料盘/语料")
+    parser.add_argument("--dest", type=str, default="W:/")
+    parser.add_argument("--threads", type=int, default=50)
+    args = parser.parse_args()
+
+    src = Path(args.src)
+    dest = Path(args.dest)
+    threadCount = args.threads
     ts = []
     for i in range(threadCount):
         ts.append(TaskThread())
+        ts[i].start()
     i = 0
     for s in src.glob("**/*"):
         d = dest / s.relative_to(src)
         if s.is_dir():
             continue
-        if i % threadCount == 0:
-            print(d)
         if d.exists() and d.stat().st_mtime == s.stat().st_mtime and d.stat().st_size == s.stat().st_size:
+            print(d, 'exists')
             continue
         if not d.parent.exists():
             d.parent.mkdir(parents=True)
-        ts[i % threadCount].add_data(s, d)
+        idx = i % threadCount
+        ts[idx].add_data(s, d)
         i += 1
     print('-'*20, i)
     for i in range(threadCount):
-        ts[i].start()
+        ts[i].add_data(None, None)
