f9f910019cebaee19955360dc82258071843d01a
passerbya <hanghang3103@163.com>
2023-12-06 18:19:19 +0800

    优化停止逻辑

--- scripts/cp.py
+++ scripts/cp.py
@@ -2,28 +2,36 @@
 # coding: utf-8
 
 import os
+import time
 import shutil
 import threading
 import argparse
 from pathlib import Path
-from queue import Queue
+from queue import Queue, LifoQueue
 
 class TaskThread(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.queue_obj = Queue()
+        self.queue_obj = LifoQueue()
+        self.is_stop = False
 
     def add_data(self, _s, _d):
         self.queue_obj.put((_s, _d))
 
+    def stop(self):
+        self.is_stop = True
+
     def run(self):
-        while True:
+        while self.queue_obj.empty():
+            time.sleep(10)
+
+        while not self.is_stop or not self.queue_obj.empty():
             (_s, _d) = self.queue_obj.get()
-            if _s is None and _d is None:
-                break
+            print(self.name, self.queue_obj.qsize())
             if _d.exists() and int(_d.stat().st_mtime) == int(_s.stat().st_mtime) and _d.stat().st_size == _s.stat().st_size:
-                print(d, 'exists')
+                #if _s.name == 'zhthchs30.zip':
+                #    print(self.name, d, 'exists', self.queue_obj.qsize())
                 continue
             if not _d.parent.exists():
                 _d.parent.mkdir(parents=True)
@@ -45,7 +53,9 @@ class TaskThread(threading.Thread):
                     print(_d)
                     shutil.copy(_s, _d)
                     os.utime(_d, (atime, mtime))
-            print(f'{_d} ok')
+            print(self.name, _d, 'ok')
+
+        print(self.name, 'stop')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -66,9 +76,11 @@ if __name__ == "__main__":
         d = dest / s.relative_to(src)
         if s.is_dir():
             continue
+        #if s.name == 'zhthchs30.zip':
+        #    print(d, s)
         idx = i % threadCount
         ts[idx].add_data(s, d)
         i += 1
     print('-'*20, i)
     for i in range(threadCount):
-        ts[i].add_data(None, None)
+        ts[i].stop()
